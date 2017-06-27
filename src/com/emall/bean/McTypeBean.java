bd40867f5b188f4d1a8b88268d913f7cee33a54a
silencedz <805701866@qq.com>
2017-06-27 20:44:08 +0800

    add McTypeBean.java
    添加商品类别实体类

---
+++ src/com/emall/bean/McTypeBean.java
@@ -0,0 +1,36 @@
+package com.emall.bean;
+
+public class McTypeBean {
+	private int typeid;
+	private String typename;
+	private int fatherid;
+	public McTypeBean() {
+		super();
+	}
+	public McTypeBean(int typeid, String typename, int fatherid) {
+		super();
+		this.typeid = typeid;
+		this.typename = typename;
+		this.fatherid = fatherid;
+	}
+	public int getTypeid() {
+		return typeid;
+	}
+	public void setTypeid(int typeid) {
+		this.typeid = typeid;
+	}
+	public String getTypename() {
+		return typename;
+	}
+	public void setTypename(String typename) {
+		this.typename = typename;
+	}
+	public int getFatherid() {
+		return fatherid;
+	}
+	public void setFatherid(int fatherid) {
+		this.fatherid = fatherid;
+	}
+	
+	
+}
