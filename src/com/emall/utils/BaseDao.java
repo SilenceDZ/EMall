4a468e4d6bf6085ad4e9898a5fb2fe014e297bae
SilenceDZ <805701866@qq.com>
2017-06-28 14:31:44 +0800

    finish BaseDao.java
    完善BaseDao.java类.

--- src/com/emall/utils/BaseDao.java
+++ src/com/emall/utils/BaseDao.java
@@ -1,9 +1,14 @@
 package com.emall.utils;
 
+import java.lang.reflect.Field;
+import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @className:BaseDao
@@ -74,5 +79,137 @@ public class BaseDao {
 			Dbutils.close(conn, ps, rs);
 		}
 		return 0;
+	}
+	/**
+	 * @Title:queryForSingle
+	 * @Description:查询当个实体类
+	 * @param sql 数据库查询语句
+	 * @param cls 目标实体类
+	 * @param params 可变参数
+	 * @return 查询实体类的对象
+	 * @throws Exception 
+	 */
+	public static <T> T queryForSingle(String sql,Class<T> cls,Object ...params){
+		Connection conn=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		conn=Dbutils.getConnection();
+		try {
+			ps=conn.prepareStatement(sql);
+			if(params!=null){
+				int i=1;
+				for (Object obj : params) {
+					ps.setObject(i++, obj);
+				}
+			}
+			rs=ps.executeQuery();
+			ResultSetMetaData rsmd=rs.getMetaData();
+			int columnCount=rsmd.getColumnCount();
+			//使用了反射实现与普通实现的区别
+			if(rs.next()){
+				//通过反射创建需要返回的类
+				T obj=cls.newInstance(); 
+				for(int i=0;i<columnCount;i++){
+					String columnName=rsmd.getColumnName(i+1).toLowerCase();
+					Object value=rs.getObject(columnName);
+					if(value==null){
+						continue;
+					}
+					if(hasField(cls, columnName)){
+						Field f=cls.getDeclaredField(columnName);
+						f.setAccessible(true);
+						//处理特殊数据类型
+						if(value instanceof BigDecimal){
+							BigDecimal val=(BigDecimal) value;
+							if(f.getType().getName().equals("int")){
+								f.set(obj, val.intValue());
+							}else{
+								f.set(obj, val.doubleValue());
+							}
+						}else{
+							f.set(obj, value);
+						}
+					}
+				}
+				return obj;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			Dbutils.close(conn, ps, rs);
+		}
+		return null;
+	}
+	/**
+	 * @Title:baseQuery
+	 * @Description:查询实体类集合
+	 * @param sql 查询语句
+	 * @param cls 实体类
+	 * @param params 可变参数
+	 * @return 查询对象的数据集合
+	 * @throws Exception
+	 */
+	public static <T> List<T> baseQuery(String sql,Class<T> cls,Object ...params){
+		Connection conn=null;
+		PreparedStatement ps=null;
+		ResultSet rs=null;
+		List<T> list=new ArrayList<>();
+		conn=Dbutils.getConnection();
+		try {
+			ps=conn.prepareStatement(sql);
+			if(params!=null){
+				int i=1;
+				for (Object obj : params) {
+					ps.setObject(i++, obj);
+				}
+			}
+			rs=ps.executeQuery();
+			ResultSetMetaData rsmd=rs.getMetaData();
+			int columnCount=rsmd.getColumnCount();
+			//使用了反射实现与普通实现的区别
+			if(rs.next()){
+				//通过反射创建需要返回的类
+				T obj=cls.newInstance(); 
+				for(int i=0;i<columnCount;i++){
+					String columnName=rsmd.getColumnName(i+1).toLowerCase();
+					Object value=rs.getObject(columnName);
+					if(value==null){
+						continue;
+					}
+					if(hasField(cls, columnName)){
+						Field f=cls.getDeclaredField(columnName);
+						f.setAccessible(true);
+						//处理特殊数据类型
+						//因为在oracle数据库中int类型时以浮点类型保存的,所以需要根据JavaBean的具体数据类型来转换类型
+						if(value instanceof BigDecimal){
+							BigDecimal val=(BigDecimal) value;
+							if(f.getType().getName().equals("int")){
+								f.set(obj, val.intValue());
+							}else{
+								f.set(obj, val.doubleValue());
+							}
+						}else{
+							f.set(obj, value);
+						}
+					}
+				}
+				list.add(obj);
+			}
+			return list;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			Dbutils.close(conn, ps, rs);
+		}
+		return null;
+	}
+	private static <T> boolean hasField(Class<T> cls, String columnName){
+		Field[] fields=cls.getDeclaredFields();
+		for (Field field : fields) {
+			if(field.getName().equals(columnName)){
+				return true;
+			}
+		}
+		return false;
 	}
 }
